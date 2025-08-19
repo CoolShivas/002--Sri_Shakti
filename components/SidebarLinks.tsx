9f2fcfbb526b3e068c8cd77e4287998dc7a4fbed
CoolShivas <shivas2710cool00@gmail.com>
2025-08-19 12:18:00 +0530

    Completion of SidebarLinks Quick Links.

--- components/SidebarLinks.tsx
+++ components/SidebarLinks.tsx
@@ -1,14 +1,41 @@
+import Link from "next/link";
+
 export default function SidebarLinks() {
   return (
     <aside className="space-y-6">
       <div className="card p-3">
-        <h5 className="font-semibold mb-2">Quick Links</h5>
-        <ul className="divide-y">Lists of Quick links.</ul>
+        <h5 className="font-bold mb-2 text-2xl ml-10 text-sky-700">
+          Quick Links
+        </h5>
+        <ul className="divide-y ml-6 text-xl p-2 font-semibold">
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/schooluniform">School Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/womenuniform">Women's Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/menuniform">Men's Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/companyuniform">Company Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/hospitaluniform">Hospital Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/hoteluniform">Hotel Uniforms</Link>
+          </li>
+          <li className="p-2 hover:text-sky-500 hover:text-2xl">
+            <Link href="/otheruniform">Other Uniforms</Link>
+          </li>
+        </ul>
       </div>
-      <div className="card p-3 bg-red-300">
+
+      {/* <div className="card p-3 bg-red-300">
         <h5 className="font-semibold mb-2">Recommendations</h5>
         <ul className="space-y-2 text-sm">Lists of Recommendated links.</ul>
-      </div>
+      </div> */}
     </aside>
   );
 }
