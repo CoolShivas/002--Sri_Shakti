a596b3dd3cd1e06ad6f965f34dfbe1cab4c6fd62
CoolShivas <shivas2710cool00@gmail.com>
2025-08-20 15:42:20 +0530

    Formation of fetchUniformApiServer function with extraReducers.

--- app/redux/slice.js
+++ app/redux/slice.js
@@ -1,8 +1,20 @@
-const { createSlice, nanoid } = require("@reduxjs/toolkit");
+const { createSlice, nanoid, createAsyncThunk } = require("@reduxjs/toolkit");
 
 const initialState = {
   uniformArr: [],
 };
+
+///////**** Starting of creating the fetch api function with export *****///////
+export const fetchUniformApiServer = createAsyncThunk(
+  "fetchUniformApiServer",
+  async () => {
+    const response = await fetch(
+      "https://backend-kwvs.onrender.com/api/uniforms/"
+    );
+    return response.json();
+  }
+);
+///////**** Ending of creating the fetch api function with export *****///////
 
 const ApiSlice = createSlice({
   name: "uniformAPIes",
@@ -15,6 +27,12 @@ const ApiSlice = createSlice({
       };
       state.uniformArr.push(data);
     },
+  },
+  // // Formation of extraReducers for fetching api;
+  extraReducers: (builder) => {
+    builder.addCase(fetchUniformApiServer.fulfilled, (state, action) => {
+      (state.isloading = false), (state.uniformArr = action.payload);
+    });
   },
 });
 
