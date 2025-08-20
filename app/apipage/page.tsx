d85d9eabdc94ce67d2ef93947f86bb9ab845ccd1
CoolShivas <shivas2710cool00@gmail.com>
2025-08-20 17:15:47 +0530

    With the help of useEffect rendering the data instead of btn click.

--- app/apipage/page.tsx
+++ app/apipage/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUniformApiServer } from "../redux/slice";
 
@@ -29,9 +29,13 @@ const ApiPage = () => {
 
   // // // Getting data after btn click on console as :- (17) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
 
-  const handlerOnGetApiBtn = () => {
-    dispatch(fetchUniformApiServer()); // Attaching the action with the btn;
-  };
+  // const handlerOnGetApiBtn = () => {
+  //   dispatch(fetchUniformApiServer()); // Attaching the action with the btn;
+  // };
+
+  useEffect(() => {
+    dispatch(fetchUniformApiServer());
+  }, []);
 
   return (
     <>
@@ -39,12 +43,26 @@ const ApiPage = () => {
         <h1 className="font-bold text-3xl p-3 mr-5">
           Welcome to the Fetch Api Page
         </h1>
-        <button
+        {/* <button
           className="bg-sky-400 p-3 text-white text-xl rounded-full hover:bg-green-500"
           onClick={handlerOnGetApiBtn}
         >
           Get Api Data
-        </button>
+        </button> */}
+
+        {getFetchApiData.map((curElem, index) => {
+          return (
+            <div key={index}>
+              <div>
+                <h1>{curElem.title}</h1>
+                <p>{curElem.uniformCode}</p>
+                <img src={curElem.image} alt="image not found" />
+                <p>{curElem.uniformType}</p>
+                <h1>{curElem.description}</h1>
+              </div>
+            </div>
+          );
+        })}
       </center>
     </>
   );
