b1b354a652bb2b05877189a4f998247b5dc5359d
CoolShivas <shivas2710cool00@gmail.com>
2025-08-20 15:29:08 +0530

    Formation of ApiPage to test the apis in it.

---
+++ app/apipage/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ApiPage = () => {
+  return (
+    <>
+      <center>
+        <h1 className="font-bold text-3xl p-3 mr-5">
+          Welcome to the Fetch Api Page
+        </h1>
+        <button className="bg-sky-400 p-3 text-white text-xl rounded-full hover:bg-green-500">
+          Get Api Data
+        </button>
+      </center>
+    </>
+  );
+};
+
+export default ApiPage;
